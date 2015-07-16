7432a7e9e3f02fff85827caae95c52010b7a9720
Alistair Bayley <alistair@abayley.org>
2015-07-17 08:07:45 +1200

    WIP

--- Main.hs
+++ Main.hs
@@ -30,18 +30,11 @@ timesheet files - one per week?
 store in ~/.todo/
 store tasks in ~/.todo/todo.txt
 
-commands
-list : show tasks via pager, like git log
-add : create new task (at top of list?)
-del : remove task from list
-t in [task]  record starting on task. Ends current task (if any).
-t out/off  stop working altogether.
-
-We recommend that the task list is in todo.txt format:
-https://github.com/ginatrapani/todo.txt-cli/wiki/The-Todo.txt-Format
-
+I recommend that the task list is in todo.txt format:
+  https://github.com/ginatrapani/todo.txt-cli/wiki/The-Todo.txt-Format
 but this is not required (we just copy the entire task to
-the timesheet), but it is likely to make reporting easier
+the timesheet).
+It is likely to make reporting easier
 e.g producing a summary by project.
 -}
 
@@ -97,9 +90,9 @@ dayformat = "%F"
 
 
 timeDir :: FilePath
--- timeDir = "./"
+timeDir = "./"
 -- timeDir = "/c/Users/alistair/Documents/haskell/punchclock/"
-timeDir = "/home/abayley/.todo/"
+-- timeDir = "/home/abayley/.todo/"
 
 -- taskFile = "~/.timesheets/tasks.txt"
 taskFile :: String
@@ -145,16 +138,16 @@ todoFilePath = do
 
 -- construct a ZonedTime from the current day plus user-supplied time
 makeUserTime :: String -> IO (Maybe LocalTime.ZonedTime)
+-- empty string returns current time
+makeUserTime [] = LocalTime.getZonedTime >>= return . Just
+-- Nothing means the parse failed.
 makeUserTime str = do
     now <- LocalTime.getZonedTime
-    if List.null str
-    then return (Just now)
-    else do
-        let ds = List.filter Char.isDigit str
-        let mbTod = FormatTime.parseTimeM True FormatTime.defaultTimeLocale "%H%M" ds
-        let day = LocalTime.localDay (LocalTime.zonedTimeToLocalTime now)
-        let newZT = now { LocalTime.zonedTimeToLocalTime = LocalTime.LocalTime day (Maybe.fromJust mbTod) }
-        return (maybe Nothing (Just . const newZT) mbTod)
+    let ds = List.filter Char.isDigit str
+    let mbTod = FormatTime.parseTimeM True FormatTime.defaultTimeLocale "%H%M" ds
+    let day = LocalTime.localDay (LocalTime.zonedTimeToLocalTime now)
+    let newZT = now { LocalTime.zonedTimeToLocalTime = LocalTime.LocalTime day (Maybe.fromJust mbTod) }
+    return (maybe Nothing (Just . const newZT) mbTod)
 
 
 timesheetStart :: Calendar.Day -> Calendar.Day
@@ -202,19 +195,25 @@ parseTimesheet filePath = do
     return (map parseTimeEvent lns)
 
 
+stripTaskPriority :: Text.Text -> Text.Text
+stripTaskPriority task =
+    let re = (Regex.matchRegex (Regex.mkRegex "^\\([A-Z]\\) ") (Text.unpack task))
+    in case re of
+        Nothing -> task
+        Just _ -> Text.drop 4 task
+
+
 getTaskByPos :: [Text.Text] -> Int -> Either String Text.Text
 getTaskByPos tasks n = if n > 0 && n <= length tasks
-    then Right (tasks !! (n-1))
+    then Right (stripTaskPriority (tasks !! (n-1)))
     else Left ("No task matching: " ++ show n)
-
-
---getTaskByPosM :: [Text.Text] -> Int -> Text.Text
---getTaskByPosM tasks n = either fail return (getTaskByPos tasks n)
 
 
 -- If we don't find a matching task return the match text
 getTaskByToken :: [Text.Text] -> String -> Either String Text.Text
-getTaskByToken tasks str = maybe (Left ("No task matching: " ++ str)) Right
+getTaskByToken tasks str = maybe
+        (Left ("No task matching: " ++ str))
+        (Right . stripTaskPriority)
         (headMaybe matches)
     where matches = List.filter (Text.isInfixOf (Text.pack str)) tasks
 
@@ -235,10 +234,12 @@ getCurrentTask ts = snd (last ts)
 
 
 -- reverse the list and return the first item with a non-empty task
-getLastTask :: [TimeEvent] -> Text.Text
-getLastTask [] = Text.empty
+getLastTask :: [TimeEvent] -> Either String Text.Text
+getLastTask [] = Left "No previous task"
 getLastTask ts = let t = filter (not . Text.null) . reverse . map snd $ ts in
-    if List.null t then Text.empty else head t
+    if List.null t
+    then Left "No previous task"
+    else Right (head t)
 
 
 getCurrentTaskTime :: [TimeEvent] -> Maybe LocalTime.ZonedTime
@@ -275,10 +276,17 @@ timeentryDay (start, _, _) =
     LocalTime.localDay (LocalTime.zonedTimeToLocalTime start)
 
 
+timeentryFormat :: TimeEntry -> String
+timeentryFormat te@(start, end, task) =
+    let toStr = FormatTime.formatTime FormatTime.defaultTimeLocale "%H:%M"
+    in Printf.printf "  %s-%s  %5.2f  %s" (toStr start) (toStr end)
+        (timeentryDuration te) (Text.unpack task)
+
+
 -- would use [\\S] but regex-compat doesn't recognise it
--- i.e. codeRegex prefix = "\\+" ++ prefix ++ "([\\S]+)"
+-- i.e. codeRegex prefix = "\\+" ++ prefix ++ "-([\\S]+)"
 codeRegex :: String -> String
-codeRegex prefix = "\\+" ++ prefix ++ "([^ \\t\\n\\r\\f\\v]+)"
+codeRegex prefix = "\\+" ++ prefix ++ "-([^ \t\n\r\f\v]+)"
 
 
 matchTimeCode :: String -> String -> String -> String
@@ -340,22 +348,15 @@ parseDay :: String -> Maybe Calendar.Day
 parseDay = FormatTime.parseTimeM True FormatTime.defaultTimeLocale dayformat
 
 
-parseReportArgs :: Calendar.Day -> [String] -> Either String (Calendar.Day, ReportType)
-parseReportArgs monday [] = return (monday, Code)
-parseReportArgs monday [s] =
-    case parseReportType s of
-        Nothing ->
-            case parseDay s of
-                Nothing -> fail ("Date parse failed for " ++ s)
-                Just d -> return (d, Code)
-        Just t -> return (monday, t)
-parseReportArgs _ (by:day:_) =
-    case parseReportType by of
-        Nothing -> fail ("invalid report type: " ++ by)
-        Just t ->
-            case parseDay day of
-                Nothing -> fail ("Date parse failed for " ++ day)
-                Just d -> return (d, t)
+parseReportArgs :: Calendar.Day -> [String] -> (Calendar.Day, String)
+parseReportArgs monday [] = (monday, "")
+parseReportArgs monday [a] = if isDate a
+    then (Maybe.fromJust (parseDay a), "")
+    else (monday, a)
+parseReportArgs monday (a:b:_) = if isDate a
+    then (Maybe.fromJust (parseDay a), b)
+    else (monday, a)
+
 
 -- validate:
 -- timestamps are always increasing
@@ -369,7 +370,7 @@ validate (e1:more1) = go e1 more1 where
     go (_, t) [] = if Text.null t then Nothing
         else Just "Did not finish with punch-out"
     go (s1, t1) (e2@(s2, t2):more) = case () of
-      _ | s1 >= s2 -> Just ("entry " ++ show s1 ++ " followed by " ++ show s2)
+      _ | s1 >= s2 -> Just ("entry " ++ show s1 ++ " followed by earlier time: " ++ show s2)
         | Text.null t1 && Text.null t2 -> Just ("2 empty (punch-out) entries in a row: " ++ show s1 ++ ", " ++ show s2)
         | otherwise -> go e2 more
 
@@ -386,19 +387,52 @@ createTimeEntries (e1:more1) = go e1 more1 where
         else (s1, s2, t) : go e more
 
 
-doReport :: Calendar.Day -> ReportType -> String -> IO ()
-doReport start by prefix = do
+doSapReport :: Calendar.Day -> String -> [TimeEntry] -> IO ()
+doSapReport start prefix ts = do
+    let keyf = timeentryCodePrefix "no-code" prefix
+    let table = List.foldl' (accumTime keyf) Map.empty ts
+    printReport start table
+
+
+doTaskReport :: [TimeEntry] -> IO ()
+doTaskReport ts = do
+    let ds = map (\te -> (timeentryDuration te, thd3 te)) ts
+    -- sort and group by task (snd tuple element)
+    let comp a b = Ord.compare (snd a) (snd b)
+    let grp a b = snd a == snd b
+    let groups :: [[(Double, Text.Text)]]
+        groups = List.groupBy grp . List.sortBy comp $ ds
+    let f :: [(Double, Text.Text)] -> (Text.Text, Double)
+        f l = (snd (head l), sum (List.map fst l))
+    let sums = List.map f groups
+    Monad.forM_ sums $ \(task, total) ->
+        putStrLn ((Printf.printf "%6.2f" total) ++ "  " ++ Text.unpack task)
+
+
+doDayReport :: [Calendar.Day] -> [TimeEntry] -> IO ()
+doDayReport [] _ = return ()
+doDayReport (d:ds) ts = do
+    let todays = filter ((d ==) . timeentryDay) ts
+    Monad.when (not (List.null todays)) (do
+        putStrLn (show d)
+        Monad.mapM_ (putStrLn .timeentryFormat) todays
+        putStrLn ""
+        )
+    doDayReport ds ts
+
+
+-- SAP: a line per code, a column for each day
+-- task: a line per task
+-- day: a line per entry: shows start, end, task. Header per day.
+doReport :: Calendar.Day -> String -> IO ()
+doReport start prefix = do
     let fp = timesheetFilepath start
     tevs <- parseTimesheet fp
     let ts = createTimeEntries tevs
-    let keyf = case by of
-            Code -> timeentryCodePrefix "no-code" prefix
-            Task -> Text.unpack . thd3
-    Monad.forM_ ts $ \te ->
-        putStrLn (show te ++ " " ++ Printf.printf "%.4f" (timeentryDuration te) ++ " " ++ (keyf te))
-    let table = List.foldl' (accumTime keyf) Map.empty ts
-    -- print table
-    printReport start table
+    case prefix of
+        ":day" -> doDayReport (weekDays start) ts
+        ":task" -> doTaskReport ts
+        _ -> doSapReport start prefix ts
 
 
 -----------------------------------------------------------
@@ -420,12 +454,13 @@ cmdList str = do
 
 
 cmdAdd :: String -> IO ()
-cmdAdd task = do
-    Monad.unless (List.null task) (do
+cmdAdd task =
+    if List.null task
+    then putStrLn "no task given"
+    else do
         tasks <- readLines taskFile
         writeLines taskFile (List.sort (Text.pack task : tasks))
-        )
-    cmdList ""
+        cmdList ""
 
 
 cmdDel :: String -> IO ()
@@ -433,61 +468,91 @@ cmdDel str = do
     let n = maybe 0 id (Read.readMaybe str)
     lns <- readLines taskFile
     if (n > 0 && n <= List.length lns)
-    then writeLines taskFile (removeItem n lns)
+    then do
+        writeLines taskFile (removeItem n lns)
+        cmdList ""
     else putStrLn ("task index " ++ (show n) ++ " out of range (1-" ++ show (List.length lns) ++ ")")
-    cmdList ""
 
 
--- If the only arg is a number, assume it is a start time and restart the previous task.
+-- why can't I used \\d for digit? argh
+isTime :: String -> Bool
+isTime str = maybe False (const True)
+    (Regex.matchRegex (Regex.mkRegex "[0-9][0-9][:\\.][0-9][0-9]") str)
+
+
+-- ISO format: yyyy-mm-dd
+isDate :: String -> Bool
+isDate str = maybe False (const True)
+    (Regex.matchRegex (Regex.mkRegex "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]") str)
+
+
 cmdIn :: [String] -> IO ()
-cmdIn [] = putStrLn "no task given"
-cmdIn [str] =
-    let n :: Maybe Int
-        n = Read.readMaybe str
-    in maybe (doCommandIn str "") (doCommandIn "" . show) n
-cmdIn (str:time:_) = doCommandIn str time
+cmdIn [] = doCommandIn "" ""
+-- Try to detect if either of the first 2 args is a time
+cmdIn [str] = if isTime str
+    then doCommandIn "" str
+    else doCommandIn str ""
+cmdIn (str1:str2:_) = if isTime str1
+    then doCommandIn str2 str1
+    else doCommandIn str1 str2
 
 
 doCommandIn :: String -> String -> IO ()
 doCommandIn str time = do
     (fp, ts) <- getCurrentTimesheet
     tasks <- readLines taskFile
-    if List.null str
-    --let task = if List.null str then getLastTask ts else getTask str tasks
+    mbnow <- makeUserTime time
+    let ete = doIn2 str time ts tasks mbnow
+    case ete of
+        Left e -> putStrLn e
+        Right te -> do
+            putStrLn ("start: " ++ Text.unpack (snd te))
+            writeTimeSheet fp (ts ++ [te])
 
-    if getCurrentTask ts == task
-    then putStrLn "task already in progress"
-    else do
-        mbnow <- makeUserTime time
-        case mbnow of
-            Nothing -> putStrLn ("not a valid start time: " ++ time)
-            Just now -> do
-                -- FIXME check timestamp is after last entry
-                putStrLn ("start: " ++ Text.unpack task)
-                writeTimeSheet fp (ts ++ [(now, task)])
+
+doIn2 :: String -> String -> [TimeEvent] -> [Text.Text] -> Maybe LocalTime.ZonedTime -> Either String TimeEvent
+doIn2 str time ts tasks mbnow = do
+    -- if the task string is empty then reuse the last task
+    task <- if List.null str then getLastTask ts
+        else getTask str tasks
+    Monad.when (getCurrentTask ts == task)
+        (fail "task already in progress")
+    case mbnow of
+        Nothing -> fail ("not a valid start time: " ++ time)
+        Just now -> do
+            let mbprev = getCurrentTaskTime ts
+            case mbprev of
+                Nothing -> return (now, task)
+                Just prev ->
+                    if prev >= now
+                    then fail ("Start time must be after previous event: " ++ show prev)
+                    else return (now, task)
 
 
 cmdOut :: String -> IO ()
 cmdOut time = do
     (fp, ts) <- getCurrentTimesheet
-    -- We end the current task by writing a line with
-    -- just the timestamp - no task.
-    -- So if the last line (the current task) is already
-    -- empty, don't do it again.
     mbnow <- makeUserTime time
     case mbnow of
         Nothing -> putStrLn ("end time not valid: " ++ time)
         Just now -> do
             let task = getCurrentTask ts
-            putStrLn ("stop: " ++ Text.unpack task)
-            either putStrLn (\new -> writeTimeSheet fp (ts ++ [new])) (doCommandOut ts now)
+            either putStrLn
+                (\new -> do
+                    putStrLn ("stop: " ++ Text.unpack task)
+                    writeTimeSheet fp (ts ++ [new])
+                )
+                (doCommandOut ts now)
 
 
 doCommandOut :: [TimeEvent] -> LocalTime.ZonedTime -> Either String TimeEvent
 doCommandOut ts now = do
     let task = getCurrentTask ts
-    Monad.when (Text.null task)
-        (fail "no current active task")
+    -- We end the current task by writing a line with
+    -- just the timestamp - no task.
+    -- So if the last line (the current task) is already
+    -- empty, don't do it again.
+    Monad.when (Text.null task) (fail "no current active task")
     let new = (now, Text.empty)
     case getCurrentTaskTime ts of
         Nothing -> return new
@@ -495,6 +560,11 @@ doCommandOut ts now = do
             then (fail "Given time precedes last entry")
             else return new
 
+
+cmdCurrent :: String -> IO ()
+cmdCurrent _ = do
+    (_, ts) <- getCurrentTimesheet
+    TextIO.putStrLn (getCurrentTask ts)
 
 
 -- what can we validate?
@@ -508,29 +578,28 @@ cmdValidate str = do
     let fp = timesheetFilepath start
     ts <- parseTimesheet fp
     maybe (putStrLn (fp ++ " ok")) putStrLn (validate ts)
-    -- let ts2 = List.sortBy (Ord.comparing fst) ts
 
 
--- FIXME make timecode prefix an argument
 cmdReport :: [String] -> IO ()
 cmdReport args = do
     monday <- currentTimesheetStart
-    either fail (\(start, by) -> doReport start by "timecode-")
-        (parseReportArgs monday args)
+    (uncurry doReport) (parseReportArgs monday args)
+
 
 -----------------------------------------------------------
 -- Main
 
-usage :: IO ()
-usage = do
+usage :: String -> IO ()
+usage cmd = do
     putStrLn "usage: "
-    putStrLn " add <task>"
-    putStrLn " ls <text>"
-    putStrLn " del <n>"
-    putStrLn " in <n>|<task>"
-    putStrLn " out <time>"
-    putStrLn " report [code|task] <date>"
-    putStrLn " validate <date>"
+    putStrLn "  add <task>"
+    putStrLn "  ls <text>"
+    putStrLn "  del <n>"
+    putStrLn "  in <n>|<task>"
+    putStrLn "  out <time>"
+    putStrLn "  current"
+    putStrLn "  report <date> :day|:task|<timecode-prefix>"
+    putStrLn "  validate <date>"
 
 
 main :: IO Int
@@ -538,14 +607,16 @@ main = do
     args <- Environment.getArgs
     let tailArgs = unwords (tail args)
     case args of
-        [] -> usage
+        [] -> usage ""
         _ -> case head args of
+            ('h':_) -> usage tailArgs
             ('a':_) -> cmdAdd tailArgs
             ('l':_) -> cmdList tailArgs
             ('d':_) -> cmdDel tailArgs
             ('i':_) -> cmdIn (tail args)
             ('o':_) -> cmdOut tailArgs
+            ('c':_) -> cmdCurrent tailArgs
             ('r':_) -> cmdReport (tail args)
             ('v':_) -> cmdValidate tailArgs
-            _ -> usage
+            cmd -> usage cmd
     return 0
